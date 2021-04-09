247d734fd5cda32a05e26b87b2073446c680b129
Felix Lanas <felix.lanas@gmail.com>
2021-04-09 12:18:25 +0200

    Add Canvas

--- paint.py
+++ paint.py
@@ -6,6 +6,10 @@ root.title('Paint FLX')
 root.geometry('800x800')
 root.resizable(False, False)
 
-
+my_canvas = tk.Canvas(root,
+                      width=600,
+                      height=400,
+                      bg='black')
+my_canvas.pack(pady=20)
 
 root.mainloop()
